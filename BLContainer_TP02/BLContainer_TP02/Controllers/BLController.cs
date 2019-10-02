aecebe0e2dd1434798327368e02484838ce96dc6
BrunoIFSP <bruno.o@aluno.ifsp.edu.br>
2019-10-02 18:12:49 -0300

    Listas de BL e Containers funcionando

---
+++ BLContainer_TP02/BLContainer_TP02/Controllers/BLController.cs
@@ -0,0 +1,34 @@
+﻿using BLBL_TP02.DAO;
+using BLContainer_TP02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BLBL_TP02.Controllers
+{
+    public class BLController : Controller
+    {
+        // GET: BL
+        public ActionResult Index()
+        {
+            BLDAO dao = new BLDAO();
+            IList<BL> BLs = dao.Lista();
+
+            ViewBag.BLs = BLs;
+            return View();
+        }
+        public ActionResult Form()
+        {
+            ViewBag.BL = new BL();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(BL BL)
+        {
+            return View();
+        }
+    }
+}
---
+++ BLContainer_TP02/BLContainer_TP02/Scripts/Consulta.sql
@@ -0,0 +1,4 @@
+﻿insert into BLs values ('SSZALT123','CRAFT','LOG-IN PANTANAL')
+INSERT INTO BLs VALUES ('BUSSSZ456','DHL', 'MONTE SARMIENTO')
+insert into containers values ('HLXU78945612','HC','40','SSZATL123')
+insert into containers values ('MSKU12345678','DC','20', 'BUSSSZ456')
